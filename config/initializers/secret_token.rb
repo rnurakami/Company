0452f285e4921ddf7642665f50e63a82328a2290
kazuki murakami <mariamanzini8@gamil.com>
2012-04-22 20:04:45 +0900

    Companyアプリケーションの作成　参考：「jpmobileによるモバイルサイト構築]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Company::Application.config.secret_token = '9d39860771247ec2665378a2ab89a5044de95d968acefb72feb0fc93dc29bb7b6db553c460341d59a5ae063000525dec77ea632e829e8f29d2bccea34f3d0a7b'
