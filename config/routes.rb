dbbe14653d71511a33ef37334f0ab6fd818663df
kazuki murakami <mariamanzini8@gamil.com>
2012-05-06 15:45:26 +0900

    コントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Company::Application.routes.draw do
+  get "access/index"
+
+  get "news/index"
+
+  get "about/index"
+
+  get "top/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
